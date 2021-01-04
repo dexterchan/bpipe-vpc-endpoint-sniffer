1d3edc43d8684a0a394f2c46c5b7fe7b049589c4
dexter <dexter@abc.com>
2021-01-04 18:20:40 +0800

    fixed the incoming request transformation in lambda handler

--- bpipe_endpt_sniffer/handler.py
+++ bpipe_endpt_sniffer/handler.py
@@ -4,13 +4,17 @@ import os
 from app.aws.setup import aws_service_factory
 from app.endptscanner import BpipeEndpointSniffer
 from app.messagebus import BpipeEndPointListWriter
-
+from app.setting import IncomingRequest
+from app.logging import get_logger
 SNS_ARN = os.environ["SNS_ARN"]
+logger = get_logger(__name__)
 
 awsAdapter = aws_service_factory(SNS_ARN)
 
 
 def lambda_handler(event, context):
+    incoming_request = IncomingRequest.from_request(event)
+
     endpointTag = event["sniff_tags"]
     sniffer: BpipeEndpointSniffer = None
     writer: BpipeEndPointListWriter = None
@@ -19,7 +23,7 @@ def lambda_handler(event, context):
     endpointLst = sniffer.sniff_bpipe_endpoints(
         endpointTag
     )
-
-    writer.write_bpipeendpoint_list_to_messagebus(event, endpointLst)
+    logger.info(f"Result:{str(endpointLst)}")
+    writer.write_bpipeendpoint_list_to_messagebus(incoming_request, endpointLst)
 
     return endpointLst
